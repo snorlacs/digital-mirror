eca6d24f22d7003d5135d25f40f7095769280b0b
snorlacs <dwarvenkat@gmail.com>
2016-04-13 11:56:12 +0530

    removed embedded database and added neo4j server config

--- src/main/java/digitalmirror/Application.java
+++ src/main/java/digitalmirror/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
+import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 
 @SpringBootApplication
@@ -21,7 +22,7 @@ public class Application extends Neo4jConfiguration{
 
     @Bean(destroyMethod = "shutdown")
     public GraphDatabaseService graphDatabaseService() {
-        return new GraphDatabaseFactory().newEmbeddedDatabase("target/digitalmirror.db");
+        return new SpringCypherRestGraphDatabase("http://localhost:7474/db/data/","neo4j","magicmirror");
     }
 
     public static void main(String[] args) {
