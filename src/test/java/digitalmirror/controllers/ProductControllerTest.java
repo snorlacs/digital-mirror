a20f5fda66e45f82fb0381c636babe7c59f3bb66
snorlacs <dwarvenkat@gmail.com>
2016-05-05 16:02:52 +0530

    adds unit tests for adding product

--- src/test/java/digitalmirror/controllers/ProductControllerTest.java
+++ src/test/java/digitalmirror/controllers/ProductControllerTest.java
@@ -1,10 +1,12 @@
 package digitalmirror.controllers;
 
+import digitalmirror.domain.Product;
 import digitalmirror.services.ProductService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -28,5 +30,13 @@ public class ProductControllerTest {
         String categoryName = "dummyCategory";
         productController.fetchProductsByCategory(categoryName);
         verify(productService).fetchProductByCategory(categoryName.toLowerCase());
+    }
+
+    @Test
+    public void shouldAddProductToDBViaService() throws Exception {
+        Product product = Mockito.mock(Product.class);
+        productController.addProduct(product);
+        verify(productService).addProduct(product);
+
     }
 }
